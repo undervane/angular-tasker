35ee3c353256ee359a597caac6938ea8d82d2550
Sergio Márquez <sergio@mipigu.com>
2019-03-17 21:45:26 +0100

    [master] Added now configuration + static data

--- src/app/@models/static/schedules.static.ts
+++ src/app/@models/static/schedules.static.ts
@@ -1,36 +1,105 @@
+// Static schedules, just for testing
+  export function getStaticSchedules() {
 
-  // Static schedules, just for testing
-  export function getStaticSchedules() {
+    return {
+        '2019-03-18': {
+            events: [],
+            schedules: [
+                {
+                    name: 'Park Running',
+                    icon: 'fa-tree',
+                    description: '',
+                    time: {
+                        start: new Date(2019, 3, 18, 6, 0),
+                        end: new Date(2019, 3, 18, 8, 0)
+                    }
+                }
+            ]
+        },
+        '2019-03-19': {
+            events: [],
+            schedules: [
+                {
+                    name: 'Meeting CEO',
+                    icon: 'fa-users',
+                    description: '',
+                    time: {
+                        start: new Date(2019, 3, 19, 10, 0),
+                        end: new Date(2019, 3, 19, 12, 0)
+                    }
+                }
+            ]
+        },
+        '2019-03-20': {
+            events: [],
+            schedules: [
+                {
+                    name: 'Movie: The Vampires',
+                    icon: 'fa-film',
+                    description: '',
+                    time: {
+                        start: new Date(2019, 3, 20, 1, 0),
+                        end: new Date(2019, 3, 20, 3, 0)
+                    }
+                }
+            ]
+        },
+        '2019-03-21': {
+            events: [],
+            schedules: [
+                {
+                    name: 'Cooking lessons',
+                    icon: 'fa-cookie-bite',
+                    description: '',
+                    time: {
+                        start: new Date(2019, 3, 21, 13, 0),
+                        end: new Date(2019, 3, 21, 15, 0)
+                    }
+                }
+            ]
+        },
+        '2019-03-22': {
+            events: [],
+            schedules: [
+                {
+                    name: 'My Birthday',
+                    icon: 'fa-birthday-cake',
+                    description: '',
+                    time: {
+                        start: new Date(2019, 3, 22, 17, 0),
+                        end: new Date(2019, 3, 22, 20, 0)
+                    }
+                }
+            ]
+        },
+        '2019-03-23': {
+            events: [],
+            schedules: [
+                {
+                    name: 'Go Paris!',
+                    icon: 'fa-map-marked-alt',
+                    description: '',
+                    time: {
+                      start: new Date(2019, 3, 23, 5, 0),
+                      end: new Date(2019, 3, 23, 8, 0)
+                    }
+                }
+            ]
+        },
+        '2019-03-24': {
+            events: [],
+            schedules: [
+                {
+                    name: 'Visit Tour Eiffel',
+                    icon: 'fa-archway',
+                    description: '',
+                    time: {
+                      start: new Date(2019, 3, 24, 11, 0),
+                      end: new Date(2019, 3, 24, 12, 0)
+                    }
+                }
+            ]
+        },
 
-    return {
-        '2019-03-11': {
-            events: [],
-            schedules: []
-        },
-        '2019-03-12': {
-            events: [],
-            schedules: []
-        },
-        '2019-03-13': {
-            events: [],
-            schedules: []
-        },
-        '2019-03-14': {
-            events: [],
-            schedules: []
-        },
-        '2019-03-15': {
-            events: [],
-            schedules: []
-        },
-        '2019-03-16': {
-            events: [],
-            schedules: []
-        },
-        '2019-03-17': {
-            events: [],
-            schedules: []
-        },
-
-    }
+    }
 }
